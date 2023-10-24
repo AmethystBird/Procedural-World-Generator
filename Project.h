29d0fce295dd87f67497994a94832af41cefcfa0
Nikita The Wyvern <48954549+AmethystBird@users.noreply.github.com>
2023-10-24 17:22:37 +0100

    Small commenting changes

--- Project.h
+++ Project.h
@@ -12,5 +12,5 @@ int runTests(std::string value);
 
 void loadTexture(GLuint& texture, std::string texturepath);
 
-//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+//Called on mouse movement
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
