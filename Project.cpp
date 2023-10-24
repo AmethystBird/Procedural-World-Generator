f44c6c9d5c5953d3c4fc7a91c43bbc5a3842d909
Nikita The Wyvern <48954549+AmethystBird@users.noreply.github.com>
2023-10-24 17:20:15 +0100

    Recommented for demonstration lecture

--- Project.cpp
+++ Project.cpp
@@ -1,29 +1,30 @@
-//////////////////////////////////////////////////////////////////////////////
-//
-//  Triangles.cpp
-//
-//////////////////////////////////////////////////////////////////////////////
-
+//GLEW
 #include "GL/glew.h"
 #include "GL/freeglut.h"
 #include "GLFW/glfw3.h"
+
+//SHADERS
 #include "LoadShaders.h"
+
+//GLM
 #include <glm/glm.hpp> //includes GLM
 #include "glm/ext/vector_float3.hpp"
 #include "glm/fwd.hpp"
 #include <glm/ext/matrix_transform.hpp> // GLM: translate, rotate
 #include <glm/ext/matrix_clip_space.hpp> // GLM: perspective and ortho 
 #include <glm/gtc/type_ptr.hpp> // GLM: access to the value_ptr
+
+//STB
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+//GENERAL
 #include <iostream>
-#include<vector>
+#include <vector>
 
+//SPECIALISED
 #include "Project.h"
-
 #include "FastNoiseLite.h"
-
 #include "FileReader.h"
 
 using namespace std;
@@ -31,15 +32,18 @@ using namespace std;
 // to use this example you will need to download the header files for GLM put them into a folder which you will reference in
 // properties -> VC++ Directories -> libraries
 
+//VAO vertex attribute positions in correspondence to vertex attribute type
 enum VAO_IDs { Triangles, Indices, Colours, Tex, NumVAOs = 2 };
-enum Buffer_IDs { ArrayBuffer, NumBuffers = 4 };
-enum Attrib_IDs { vPosition = 0, cPosition = 1, tPosition = 2 };
-
+//VAOs
 GLuint  VAOs[NumVAOs];
-GLuint  Buffers[NumBuffers];
-GLuint texture1;
 
-//const GLuint  NumVertices = 24; //OG: 36
+//Buffer types
+enum Buffer_IDs { ArrayBuffer, NumBuffers = 4 };
+//Buffer objects
+GLuint  Buffers[NumBuffers];
+
+enum Attrib_IDs { vPosition = 0, cPosition = 1, tPosition = 2 };
+GLuint texture1;
 
 //----------------------------------------------------------------------------
 //
@@ -51,7 +55,7 @@ GLuint program;
 
 bool drawWireframe = false;
 
-//Screen dimensions
+//Window
 #define SCREEN_SIZE_X 1280
 #define SCREEN_SIZE_Y 720
 
@@ -68,62 +72,40 @@ const int trianglesPerSquare = 2;
 const int trianglesRow = squaresRow * trianglesPerSquare;
 const int trianglesGrid = trianglesRow * squaresRow;
 
-//Camera
+//Transformations
+//Relative position within world space
 glm::vec3 cameraPosition = glm::vec3(0.0f, 0.0f, 3.0f);
+//The direction of travel
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+//Up position within world space
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-//Field of vision
+//Scene perception construction; model & view
 glm::mat4 model;
 glm::mat4 projection;
 
-//Direction
+//Camera sideways rotation
 float yaw = -90.0f;
+//Camera vertical rotation
 float pitch = 0.0f;
+//Determines if first entry of mouse into window
 bool mouseFirstEnter = true;
+//Positions of camera from given last frame
 float lastX = 800.0f / 2.0f;
 float lastY = 600.0f / 2.0f;
+//Field of vision
 float fov = 45.0f;
 
 //Time
+//Time change
 float deltaTime = 0.0f;
+//Last value of time change
 float lastFrame = 0.0f;
-
-/*struct Vertex {
-	GLfloat position[3];
-	GLfloat normal[3];
-};
-
-class Mesh {
-public:
-	vector<Vertex> vertices;
-	vector<unsigned int> indices;
-
-	Mesh(vector<Vertex> vertices, vector<unsigned int> indices);
-	void Draw();
-private:
-	unsigned int VAO, VBO, EBO;
-	void SetupMesh();
-};
-
-class Model
-{
-public:
-	Model(char* path)
-	{
-		LoadModel(path);
-	}
-	void Draw();
-private:
-	vector<Mesh> meshes;
-	string directory;
-
-	void LoadModel(string path);
-};*/
 
 void
 init(void)
 {
+	//Load shaders
 	ShaderInfo  shaders[] =
 	{
 		{ GL_VERTEX_SHADER, "media/mapShader.vert" },
@@ -214,7 +196,6 @@ init(void)
 		mapIndices[i][1] = 1 + indexColumnShift + indexRowShift; //top right
 
 		mapIndices[i + 1][0] = 1 + indexColumnShift + indexRowShift; //top right
-		//mapIndices[i + 1][1] = 1 + RENDER_DISTANCE + indexColumnShift + indexRowShift; //bottom right
 		mapIndices[i + 1][2] = RENDER_DISTANCE + indexColumnShift + indexRowShift; //bottom left
 		mapIndices[i + 1][1] = 1 + RENDER_DISTANCE + indexColumnShift + indexRowShift; //bottom right
 
@@ -229,22 +210,29 @@ init(void)
 		}
 	}
 
+	//Sets index of VAO
 	glGenVertexArrays(NumVAOs, VAOs);
+	//Binds VAO to a buffer
 	glBindVertexArray(VAOs[0]);
 
+	//Sets indexes of all required buffer objects
 	glGenBuffers(NumBuffers, Buffers);
 
-	//Assigning map vertices to buffer
+	//Binds vertex object (map vertices) to array buffer
 	glBindBuffer(GL_ARRAY_BUFFER, Buffers[Triangles]);
+	//Allocates buffer memory for the vertices of the 'Triangles' buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(mapVertices), mapVertices, GL_STATIC_DRAW);
 
-	//Assigning map indices to buffer
+	//Binding & allocation for map indices
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Buffers[Indices]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(mapIndices), mapIndices, GL_STATIC_DRAW);
 
-	//Interpretation of vertices/indices in memory
+	//Allocation & indexing of vertex attribute memory for vertex shader
+	//Positions
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); //positions
 	glEnableVertexAttribArray(0); //Enabling of vertex attribute; takes location
+
+	//Colours
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); //colours
 	glEnableVertexAttribArray(1);
 
@@ -252,13 +240,16 @@ init(void)
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	//Creation of the model matrix
+	//Model matrix
 	model = glm::mat4(1.0f);
+	//Scaling to zoom in
 	model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
-	model = glm::rotate(model, glm::radians(40.0f), glm::vec3(1.0f, 0.0f, 0.0f)); //OG: radians(-40.0f), vec3(1.0f, 0.0f, 0.0f)
+	//Rotation to look up
+	model = glm::rotate(model, glm::radians(40.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	//Movement to reposition
 	model = glm::translate(model, glm::vec3(0.0f, 0.0f, -1.0f));
 
-	//Creation of the projection matrix
+	//Projection matrix
 	projection = glm::perspective(glm::radians(fov), (float)SCREEN_SIZE_X / (float)SCREEN_SIZE_Y, 0.1f, 100.0f);
 
 	//MAY BE PROBLEMATIC
@@ -310,7 +301,10 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
 void processInput(GLFWwindow* window)
 {
+	//Extent to which to move in one instance
 	const float cameraMovementSpeed = 0.5f * deltaTime;
+
+	//WASD controls
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) //Forward
 	{
 		cameraPosition += cameraMovementSpeed * cameraFront;
@@ -327,28 +321,25 @@ void processInput(GLFWwindow* window)
 	{
 		cameraPosition -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraMovementSpeed;
 	}
+	//Closes window on 'exit' key press
 	else if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) //Direction
 	{
 		glfwSetWindowShouldClose(window, true);
 	}
-	else if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) //Switch between triangle & wireframe rendering modes
+	//Switch between triangle & wireframe rendering modes
+	else if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
 	{
 		drawWireframe = !drawWireframe;
 	}
 }
 
-//Unused
+//Load and create a texture; unused
 void loadTexture(GLuint& texture, std::string texturepath)
 {
-	// load and create a texture 
-// -------------------------
-
-// texture 1
-// ---------
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	// set texture filtering parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -369,28 +360,22 @@ void loadTexture(GLuint& texture, std::string texturepath)
 	stbi_image_free(data);
 }
 
-
-//----------------------------------------------------------------------------
-//
-// display
-//
-
-
 void
 display(void)
 {
 	static const float black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
 
 	glClearBufferfv(GL_COLOR, 0, black);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT); //Clears the colour buffer
+
 	// bind textures on corresponding texture units
 	glFrontFace(GL_CW);
 	glCullFace(GL_BACK);
 	glEnable(GL_CULL_FACE);
 
-	//camera position
+	//Transformations
 	glm::mat4 view;
-	view = glm::lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp);
+	view = glm::lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp); //Sets the position of the viewer, the movement direction in relation to it & the world up direction
 
 	// Adding all matrices up to create combined matrix
 	glm::mat4 mvp = projection * view * model;
@@ -400,7 +385,7 @@ display(void)
 	glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(mvp));
 
 	//modify position using mv & p
-	glBindVertexArray(VAOs[0]);
+	glBindVertexArray(VAOs[0]); //Bind buffer object to render;
 
 	if (drawWireframe == false)
 	{
@@ -429,150 +414,50 @@ int runTests(std::string value)
 	// your actual program execution goes here - only if we haven't exited
 	return res; // + your_program_res
 }
-//----------------------------------------------------------------------------
-//
-// main
-//
+
 int
 main(int argc, char** argv)
 {
+	//Initialisation of GLFW
 	glfwInit();
 
+	//Initialisation of 'GLFWwindow' object
 	GLFWwindow* window = glfwCreateWindow(1280, 720, "Procedural World Generator", NULL, NULL);
 
+	//Sets cursor to automatically bind to window & hides cursor pointer
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); //Mouse capture mode
 
+	//Binds OpenGL to window
 	glfwMakeContextCurrent(window);
+	//Initialisation of GLEW
 	glewInit();
 
 	init();
 
+	//Sets the mouse_callback() function as the callback for the mouse movement event
 	glfwSetCursorPosCallback(window, mouse_callback);
 
+	//Render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		//Time
 		float currentFrame = static_cast<float>(glfwGetTime());
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		processInput(window);
+		//Input
+		processInput(window); //Takes user input
 
+		//Rendering, transformations & drawing
 		display();
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		//Refreshing
+		glfwSwapBuffers(window); //Swaps the colour buffer
+		glfwPollEvents(); //Queries all GLFW events
 	}
 
 	glfwDestroyWindow(window);
 
+	//Safely terminates GLFW
 	glfwTerminate();
 }
-
-/*Mesh::Mesh(vector<Vertex> vertices, vector<unsigned int> indices)
-{
-	this->vertices = vertices;
-	this->indices = indices;
-
-	SetupMesh();
-}
-
-void Mesh::Draw()
-{
-	unsigned int diffuseNr = 1;
-	unsigned int specularNr = 1;
-	unsigned int normalNr = 1;
-	unsigned int heightNr = 1;
-
-	// draw mesh
-	glBindVertexArray(VAO);
-	glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
-	glBindVertexArray(0);
-}
-
-void Mesh::SetupMesh()
-{
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
-		&indices[0], GL_STATIC_DRAW);
-
-	//Vertices
-	glEnableVertexAttribArray(0); //Positions
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-
-	glEnableVertexAttribArray(1); //Normals
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
-
-	glBindVertexArray(0);
-}
-
-void Model::Draw()
-{
-	for (unsigned int i = 0; i < meshes.size(); i++)
-	{
-		meshes[i].Draw();
-	}
-}
-
-void Model::LoadModel(string path)
-{
-	//Import with my own File Reader
-	FileReader OBJFileReader;
-	OBJFileReader.openFile("pogg");
-	
-	if (OBJFileReader.currentFileName == "")
-	{
-		cout << "Error loading model with FileReader.";
-	}
-
-	vector<Vertex> vertices;
-	vector<Vertex> indices;
-	//vector<Vector> textures;
-
-	bool hitEnd = false;
-
-	GLfloat loadingVertex[3];
-	for (int i = 0; hitEnd == false; i++)
-	{
-		vector<string> vertex;
-		vertex = OBJFileReader.getNextElement("vertex");
-
-		if (vertex.front() == "")
-		{
-			hitEnd = true;
-		}
-		else
-		{
-			string posX = vertex[0];
-			GLfloat posXValue = stof(posX);
-			string posY = vertex[1];
-			GLfloat posYValue = stof(posY);
-			string posZ = vertex[2];
-			GLfloat posZValue = stof(posZ);
-
-			GLfloat allPosValues[3] = {
-				posXValue, posYValue, posZValue
-			};
-
-			Vertex vertexToSend;
-			vertexToSend.position = allPosValues;
-
-			vertices[i].position = allPosValues;
-		}
-	}
-
-	bool hitEnd = false;
-
-	for (int i = 0; hitEnd == false; i++)
-	{
-
-	}
-}*/
